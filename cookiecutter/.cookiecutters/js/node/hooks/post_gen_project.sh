579eea3c20ebf404128bd262c5aa999929d60066
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-02-14 09:24:05 -0500

    Improved copying of config files from dependencies

--- cookiecutter/.cookiecutters/js/node/hooks/post_gen_project.sh
+++ cookiecutter/.cookiecutters/js/node/hooks/post_gen_project.sh
@@ -11,10 +11,16 @@ then
     cd "{{cookiecutter.dependency_dir}}"
 
     # Create new package.json by merging base with packages from dependencies
-    cat "$OLDPWD/package-base.json" `find  -L {{cookiecutter.dependencies}} -not -path '*node_modules*' -iname "package.json"` | json --deep-merge > "$OLDPWD/package.json"
+    cat "$OLDPWD/package-base.json" `find -L {{cookiecutter.dependencies}} -not -path '*node_modules*' -iname "package.json"` | json --deep-merge > "$OLDPWD/package.json"
 
-    # copy webpack config files and/or "conf" directories from dependecies
-    find . -not -path '*node_modules*' -iname 'webpack.conf*' -or -type d -iname 'conf' | xargs cp -L -r -t "$OLDPWD"
+    # Copy webpack config files
+    find {{cookiecutter.dependencies}} -not -path '*node_modules*' -iname 'webpack.conf*' -exec cp -L -r -t "$OLDPWD" '{}' \+
+
+    # Copy "rc" files
+    find {{cookiecutter.dependencies}} -not -path '*node_modules*' -iname '.*rc' -exec cp -L -r -t "$OLDPWD" '{}' \+
+
+    # Copy "conf" directories from dependecies
+    find {{cookiecutter.dependencies}} -not -path '*node_modules*' -type d -iname 'conf' -exec cp -L -r -t "$OLDPWD" '{}' \+
 
     # Switch back to project directory
     cd "$OLDPWD"
