893c31fb3717506d395d9752c9d9d59cfc2debde
Brian Dellaterra <bdellaterra@users.noreply.github.com>
2017-04-24 04:10:32 -0400

    Establish env-vars for project scaffolding.

--- skeletons/.skeletons/js/node/deps/webpack2/webpack.config.babel.js
+++ skeletons/.skeletons/js/node/deps/webpack2/webpack.config.babel.js
@@ -4,10 +4,14 @@ import merge from 'webpack-merge'
 
 import modules from './conf/webpack'
 
+// Define constants.
+const production = (process.env.NODE_ENV === 'production')
+const debug = !production
+
 const DefinePlugin = new webpack.DefinePlugin({
-  'PRODUCTION': JSON.stringify(false),
-  'VERSION':    JSON.stringify('0.1'),
-  'DEBUG':      JSON.stringify(true)
+  PRODUCTION: JSON.stringify(production),
+  VERSION:    JSON.stringify('0.1'),
+  DEBUG:      JSON.stringify(debug)
 })
 
 const baseConfig = {
@@ -19,6 +23,19 @@ const baseConfig = {
   'plugins': [
     DefinePlugin
   ]
+}
+
+// Use source maps if debugging.
+if (debug) { baseConfig.devtool = 'source-map' }
+
+// Minify if not debugging.
+if (production) {
+  baseConfig.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+     sourceMap: false,
+     compress:  { warnings: false }
+    })
+  )
 }
 
 export default merge(baseConfig, modules)
---
+++ skeletons/.skeletons/vim/skeleton.js
@@ -0,0 +1,8 @@
+// File:         `!v substitute(fnamemodify(expand('%'), ':p'), '^\V' . fnamemodify(fnamemodify(findfile('.git', '.;'), ':p:h'), ':p'), '', '' )`
+// Description:  `!v g:local.project_brief`
+// Author:       `!v g:local.author` <github.com/`!v g:local.github_user`>
+// Version:      `!v g:local.version`
+// License:      Copyright `!v strftime('%Y')` `!v g:local.author`. This file is part of `!v g:local.project_name`.
+//               `!v g:local.license_brief`
+
+
