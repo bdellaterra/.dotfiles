0027a7aa3d5e9d45959d1a7817861a8e222fec13
Brian Dellaterra <bdellaterra@users.noreply.github.com>
2017-05-01 23:03:44 -0400

    Update skeletons.

--- skeletons/.skeletons/vim/skeleton.js
+++ skeletons/.skeletons/vim/skeleton.js
@@ -1,8 +1,8 @@
 // File:         `!v substitute(fnamemodify(expand('%'), ':p'), '^\V' . fnamemodify(fnamemodify(findfile('.git', '.;'), ':p:h'), ':p'), '', '' )`
-// Description:  `!v g:local.project_brief`
-// Author:       `!v g:local.author` <github.com/`!v g:local.github_user`>
-// Version:      `!v g:local.version`
-// License:      Copyright `!v strftime('%Y')` `!v g:local.author`. This file is part of `!v g:local.project_name`.
-//               `!v g:local.license_brief`
+// Description:  `!v len($PROJECT_BRIEF) ? $PROJECT_BRIEF : ''`
+// Author:       `!v len($PROJECT_CONTACT) ? $PROJECT_CONTACT : ''`
+// Version:      `!v len($PROJECT_VERSION) ? $PROJECT_VERSION : '0.1.0'`
+// License:      `!v len($PROJECT_LICENSE_NAME) ? $PROJECT_LICENSE_NAME : 'Copyright Act of 1976, Pub.L. 94-553, 90 Stat. 2541, Section 401(a) (October 19, 1976)'`
+//               `!v len($PROJECT_LICENSE_BRIEF) ? $PROJECT_LICENSE_BRIEF : 'All rights reserved. This body of work or any portion thereof may not be reproduced or used in any manner whatsoever without the express written permission of the author.'`
 
 
