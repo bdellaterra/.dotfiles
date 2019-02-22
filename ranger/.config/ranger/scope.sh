cc531f80f56df4eee2ff3a317d3108ee8bf06162
Brian Dellaterra <bdellaterra@users.noreply.github.com>
2019-02-22 01:48:47 -0500

    fix syntax-highlighting

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -100,7 +100,11 @@ case "$mimetype" in
     text/* | */xml)
         if [ "$(tput colors)" -ge 256 ]; then
             pygmentize_format=terminal256
-            highlight_format=xterm256
+            # highlight_format=xterm256
+
+# Force ansi colorscheme for syntax-highlighting
+highlight_format=ansi
+
         else
             pygmentize_format=terminal
             highlight_format=ansi
